e1d40bead60dc5de0d85560949c8b3d85423be2d
Unknown <matasramanuaskas@gmail.com>
2018-12-03 14:17:34 +0200

    Line removed

--- myScript2.js
+++ myScript2.js
@@ -3,7 +3,7 @@ let gui;
 window.onload =  () =>
 {
     let sphereR = 10;
-    let pointsCount = 100000;
+    let pointsCount = 1000;
     // GUI
     var robotControllVars = function() {
         // this.yellowX = 0;
@@ -41,35 +41,45 @@ window.onload =  () =>
 
     scene.add( lights[ 0 ] );
     scene.add( lights[ 1 ] );
+    // helpers
+    scene.add( new THREE.AxesHelper( 20 ) );
 
     // Shapes
     var texture = new THREE.TextureLoader().load( "texture.png" );
-    // texture.wrapS = THREE.RepeatWrapping;
-    // texture.wrapT = THREE.RepeatWrapping;
-    // texture.repeat.set( 10, 10 );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 2, 2 );
     let points = generatePoints(pointsCount,sphereR);
 
     var geometry = new THREE.ConvexGeometry( points);
     assignUVs(geometry);
     var material = new THREE.MeshBasicMaterial({
         map: texture,
-        side: THREE.FrontSide
+        side: THREE.FrontSide,
+        opacity: 1,
+        transparent: true
       });
     var mesh = new THREE.Mesh( geometry, material );
     mesh.scale.x = -1;
     scene.add( mesh );
 
+    var edges = new THREE.EdgesGeometry( geometry );
+    var line = new THREE.LineSegments( edges, new THREE.LineBasicMaterial( { color: 0x0080ff, size: 0.01, } ) );
+    //scene.add( line );
+
     var pointsMaterial = new THREE.PointsMaterial( {
         color: 0x0080ff,
-        size: 1,
-        alphaTest: 0.5
+        size: 0.1,
+        alphaTest: 0.5,
+        opacity: 0.5,
+        transparent: true
     } );
 
     let group = new THREE.Group();
     var pointsGeometry = new THREE.BufferGeometry().setFromPoints( points );
     var pointsInScene = new THREE.Points( pointsGeometry, pointsMaterial );
-    group.add( pointsInScene );
-    //scene.add(group);
+    //group.add( pointsInScene );
+    scene.add(group);
   
     // End of Shapes
 
@@ -140,15 +150,6 @@ function getRandomArbitrary(min, max) {
 // Next functions are based on the code found at https://codepen.io/knee-cola/pen/XMVBwQ
 
 function assignUVs(geometry) {
-
-    // This function is based on the code found at (the original source doesn't work well)
-    // http://stackoverflow.com/questions/20774648/three-js-generate-uv-coordinate
-    // 
-    // She following page explains how UV map should be calculated
-    // https://solutiondesign.com/blog/-/blogs/webgl-and-three-js-texture-mappi-1/
-    // 
-    // The following documentation shows what a apherical UV map should look like
-    // https://threejs.org/examples/#misc_uv_tests
   
     // converting all vertices into polar coordinates    
     var polarVertices = geometry.vertices.map(cartesian2polar);
@@ -161,53 +162,76 @@ function assignUVs(geometry) {
   
         var uvs = [];
 
-        // Each face is a triangle defined by three points or vertices (point a, b and c).
-        // Instead of storing the three points (vertices) by itself,
-        // a face uses points from the [vertices] array.
-        // The 'a', 'b' and 'c' properties of the [face] object in fact represent
-        // index at which each of the three points is stored in the [vertices] array
-        var ids = [ 'a', 'b', 'c'];
+        // using the point to access the vertice
+        var vertexIndex = face[ 'a' ];
+        var vertex = polarVertices[ vertexIndex ];   
 
-        for( var i = 0; i < ids.length; i++ ) {
+        var canvasPoint1 = polar2canvas(vertex);
 
-            // using the point to access the vertice
-            var vertexIndex = face[ ids[ i ] ];
-            var vertex = polarVertices[ vertexIndex ];
+        // using the point to access the vertice
+        var vertexIndex = face[ 'b'];
+        var vertex = polarVertices[ vertexIndex ];
 
-            // If the vertice is located at the top or the bottom
-            // of the sphere, the x coordinates will always be 0
-            // This isn't good, since it will make all the faces
-            // which meet at this point use the same starting point
-            // for their texture ...
-            // this is a bit difficult to explainm, so try to comment out
-            // the following block and take look at the top of the
-            // spehere to see how it is mapped. Also have a look
-            // at the following image: https://dev.ngit.hr/vr/textures/sphere-uv.png
-            // if(vertex.theta === 0 && (vertex.phi === 0 || vertex.phi === Math.PI)) {
+        var canvasPoint2 = polar2canvas(vertex);
 
-            // // at the sphere bottom and at the top different
-            // // points are alligned differently - have a look at the
-            // // following image https://dev.ngit.hr/vr/textures/sphere-uv.png
-            // var alignedVertice = vertex.phi === 0 ? face.b : face.a;
+        var vertexIndex = face[ 'c' ];
+        var vertex = polarVertices[ vertexIndex ];
 
-            // vertex = {
-            //     phi: vertex.phi,
-            //     theta: polarVertices[ alignedVertice ].theta
-            // };
-            // }
+        var canvasPoint3 = polar2canvas(vertex);
 
-            // // Fixing vertices, which close the gap in the circle
-            // // These are the last vertices in a row, and are at identical position as
-            // // vertices which are at the first position in the row.
-            // // This causes the [theta] angle to be miscalculated
-            // if(vertex.theta === Math.PI && cartesian2polar(face.normal).theta < Math.PI/2) {
-            // vertex.theta = -Math.PI;
-            // }
+        let shiftAmount = 0.1;
 
-            var canvasPoint = polar2canvas(vertex);
+        let changed = true;
+        while (changed)
+        {
+            changed = false;
+        
+            if (Math.abs(canvasPoint1.x - canvasPoint2.x) > shiftAmount
+            && Math.abs(canvasPoint1.x - canvasPoint3.x) > shiftAmount)
+            {
+                changed= true;
+                if (canvasPoint1.x > canvasPoint2.x)
+                {
+                    canvasPoint1.x -= shiftAmount;
+                }
+                else
+                {
+                    canvasPoint1.x += shiftAmount;
+                }
 
-            uvs.push( new THREE.Vector2( /* 1 - */ canvasPoint.x,/* 1 - */  canvasPoint.y ) );
+            }
+            else if (Math.abs(canvasPoint2.x - canvasPoint3.x) > shiftAmount
+            && Math.abs(canvasPoint2.x - canvasPoint1.x) > shiftAmount)
+            {
+                changed= true;
+                if (canvasPoint2.x > canvasPoint1.x)
+                {
+                    canvasPoint2.x -= shiftAmount;
+                }
+                else
+                {
+                    canvasPoint2.x += shiftAmount;
+                }
+            }
+            else if (Math.abs(canvasPoint3.x - canvasPoint1.x) > shiftAmount
+            && Math.abs(canvasPoint3.x - canvasPoint2.x) > shiftAmount)
+            {
+                changed= true;
+                if (canvasPoint3.x > canvasPoint1.x)
+                {
+                    canvasPoint3.x -= shiftAmount;
+                }
+                else
+                {
+                    canvasPoint3.x += shiftAmount;
+                }
+            }
         }
+
+
+        uvs.push( new THREE.Vector2( canvasPoint1.x, canvasPoint1.y ) );
+        uvs.push( new THREE.Vector2(  canvasPoint2.x,canvasPoint2.y ) );
+        uvs.push( new THREE.Vector2( canvasPoint3.x,canvasPoint3.y ) );
   
         geometry.faceVertexUvs[ 0 ].push( uvs );
     });
@@ -236,4 +260,10 @@ function polar2canvas(polarPoint) {
         y: polarPoint.phi/Math.PI,
         x: (polarPoint.theta+Math.PI) / (2*Math.PI)
     })
+}
+
+function distanceAB(x1,y1,x2,y2) {
+    var a = x1 - x2;
+    var b = y1 - y2;
+    return Math.sqrt( a*a + b*b );
 }
